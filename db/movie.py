f89d5b8927428fc7817eb156b017c2d28031e542
nazarkravchenko <nazarnazarik5@gmail.com>
2026-01-29 00:44:15 +0200

    check

---
+++ db/movie.py
@@ -0,0 +1,48 @@
+from typing import Iterable, Optional
+
+from db.models import Movie
+
+
+def get_movies(
+    genres_ids: Optional[Iterable[int]] = None,
+    actors_ids: Optional[Iterable[int]] = None,
+):
+    movies = Movie.objects.all()
+
+    if genres_ids is not None and actors_ids is not None:
+        return movies.filter(
+            genres__id__in=list(genres_ids),
+            actors__id__in=list(actors_ids),
+        ).distinct()
+
+    if genres_ids is not None:
+        return movies.filter(genres__id__in=list(genres_ids)).distinct()
+
+    if actors_ids is not None:
+        return movies.filter(actors__id__in=list(actors_ids)).distinct()
+
+    return movies
+
+
+def get_movie_by_id(movie_id: int) -> Movie:
+    return Movie.objects.get(id=movie_id)
+
+
+def create_movie(
+    movie_title: str,
+    movie_description: str,
+    genres_ids: Optional[Iterable[int]] = None,
+    actors_ids: Optional[Iterable[int]] = None,
+) -> Movie:
+    movie = Movie.objects.create(
+        title=movie_title,
+        description=movie_description,
+    )
+
+    if genres_ids is not None:
+        movie.genres.set(list(genres_ids))
+
+    if actors_ids is not None:
+        movie.actors.set(list(actors_ids))
+
+    return movie
