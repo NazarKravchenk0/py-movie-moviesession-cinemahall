1e75c51111277937927a77fbb787781570e105e2
nazarkravchenko <nazarnazarik5@gmail.com>
2026-01-29 14:58:46 +0200

    check12

--- services/movie.py
+++ services/movie.py
@@ -9,7 +9,7 @@ def get_movies(
     genres_ids: Optional[Iterable[int]] = None,
     actors_ids: Optional[Iterable[int]] = None,
 ) -> QuerySet[Movie]:
-    queryset = Movie.objects.all()
+    queryset = Movie.objects.all().order_by("id")
 
     if genres_ids:
         queryset = queryset.filter(genres__id__in=list(genres_ids))
@@ -17,7 +17,7 @@ def get_movies(
     if actors_ids:
         queryset = queryset.filter(actors__id__in=list(actors_ids))
 
-    return queryset.distinct()
+    return queryset.distinct().order_by("id")
 
 
 def get_movie_by_id(movie_id: int) -> Movie:
