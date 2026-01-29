786ad962db9f9e2bf8006662934f2715ad752319
nazarkravchenko <nazarnazarik5@gmail.com>
2026-01-29 14:54:04 +0200

    check10

--- db/models.py
+++ db/models.py
@@ -29,7 +29,8 @@ class CinemaHall(models.Model):
 
 class MovieSession(models.Model):
     show_time = models.DateTimeField()
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="movie_sessions")
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE,
+                              related_name="movie_sessions")
     cinema_hall = models.ForeignKey(
         CinemaHall,
         on_delete=models.CASCADE,
@@ -42,7 +43,9 @@ class MovieSession(models.Model):
 
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="orders")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE,
+                             related_name="orders")
 
     class Meta:
         ordering = ["-created_at"]
